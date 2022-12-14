e23d748aeae5125671c217c4bb05061c8b597703
mandelakibiriti <mandelakibiriti@gmail.com>
2022-12-14 03:39:23 +0300

    sizeof.c file added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- *     
+/**
+ * main - main function
+ * Return: 0 on success
  */
 int main(void)
 {
-    return (0);
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+
+return (0);
 }
