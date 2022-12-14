ee683f06b2176eb5e7066b5e17bd9ec1f88699cf
mandelakibiriti <mandelakibiriti@gmail.com>
2022-12-14 03:25:18 +0300

    amended puts.c file removed whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- *     
+/**
+ * main - main function
+ *
+ * Return: 0 on success
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
