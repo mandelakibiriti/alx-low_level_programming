2233a8a52de238c19d6fb3d41394c29fbcfd3650
mandelakibiriti <mandelakibiriti@gmail.com>
2022-12-16 02:06:28 +0300

    amended 1-main.c script to comply with Betty linter

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
-* Return: 0
+* Return: 0 on success
 */
-
 int main(void)
 {
-        int i;
+int i;
+printf("Infinite loop incoming :(\n");
 
-        printf("Infinite loop incoming :(\n");
-
-        i = 0;
-
-        // while (i < 10)
-        // {
-        //         putchar(i);
-        // }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+i = 0;
+/*
+* while (i < 10)
+* {
+*       putchar(i);
+* }
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
